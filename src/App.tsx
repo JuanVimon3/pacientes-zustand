74efbfacdd6d27cbb56a8eebfd646322e4e017c9
JuanVimon3 <jvillamonsalve@gmail.com>
2026-03-05 17:59:40 -0500

    create PatientForm and PatientsList and install react-hook-form

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
+import PatienForm from "./components/PatienForm"
+import PatientsList from "./components/PatientsList"
+
  
 
 function App() {
   
   return (
     <>
-      <h1 className="text-4xl">Pacientes</h1>
+      <div className="container mx-auto mt-20">
+        <h1 className="font-black text-5xl text-center md:w-2/3 md:mx-auto">
+          Seguimiento de pacientes {''}
+          <span className="text-indigo-700">Veterinaria </span>
+        </h1>
+
+        <div className="mt-12 md:flex">
+          <PatienForm/>
+          <PatientsList/>
+        </div>
+      </div>
     </>
   )
 }
