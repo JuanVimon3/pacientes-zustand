e2a20d6c9549b5a9f3e15fe7a5c8395cdccba8da
JuanVimon3 <jvillamonsalve@gmail.com>
2026-03-12 16:36:19 -0500

    create PatiendDetails and PatientDetailsItem components to render patients info

--- src/components/PatientsList.tsx
+++ src/components/PatientsList.tsx
@@ -1,4 +1,5 @@
 import { usePatientStore } from "../store"
+import PatientDetails from "./PatientDetails";
 
 
 
@@ -9,6 +10,30 @@ export default function PatientsList() {
   console.log(patients);
   
   return (
-    <div>PatientsList</div>
+    <div className="md:w-1/2 lg:w-3/5  md:h-screen overflow-y-scroll">
+      {patients.length ? (
+        <>
+          <h2 className="font-black text-3xl text-center">Listado de pacientes</h2>
+          <p className="text-xl mt-5 mb-10 text-center">
+            Administra tus {''}
+            <span className="text-indigo-600 font-bold">Pacientes y citas</span>
+          </p>
+          {patients.map(patient => (
+            <PatientDetails
+              key={patient.id}
+              patient={patient}
+            />
+          ))}
+        </>
+      ) : (
+        <>
+          <h2 className="font-black text-3xl text-center">No hay pacientes</h2>
+          <p className="text-xl mt-5 mb-10 text-center">
+            Comienza agregando pacientes {''}
+            <span className="text-indigo-600 font-bold">y aparecerán en este lugar</span>
+          </p>
+        </>
+      )}
+    </div>
   )
 }
